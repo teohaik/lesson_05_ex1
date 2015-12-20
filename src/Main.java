c09367f51c0804a675c91779a4e542bf30ea1aad
Theodore Chaikalis <teohaik@gmail.com>
2015-12-20 21:42:17 +0200

    First project commit. Submitted 8 new files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Thodoris on 20-Dec-15.
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! This is a GIT - GitHub - IDEA demo!");
+
+    }
+}
